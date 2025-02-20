e0a9d20b28a491e814bcb4844a37cbeb826d765b
T0ucan <181164199+T0ucan-png@users.noreply.github.com>
2025-02-20 18:45:02 -0500

    Added C source file for when I figure out how to code a program that reads USB input and puts it in a text file...

---
+++ Win_USB_Read/Win_USB_Read.cpp
@@ -0,0 +1,10 @@
+HANDLE hComm;
+hComm = CreateFile( gszPort,  
+                    GENERIC_READ | GENERIC_WRITE, 
+                    0, 
+                    0, 
+                    OPEN_EXISTING,
+                    FILE_FLAG_OVERLAPPED,
+                    0);
+if (hComm == INVALID_HANDLE_VALUE)
+   // error opening port; abort
